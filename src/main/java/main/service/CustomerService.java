5917479c03b4fb31df76d10882a2425e2dde8e5a
ceyhunaziz <40896664+ceyhunaziz@users.noreply.github.com>
2019-09-24 01:20:16 +0400

    Add gitignore

--- src/main/java/main/service/CustomerService.java
+++ src/main/java/main/service/CustomerService.java
@@ -4,8 +4,6 @@ import main.model.Customer;
 import main.repository.CustomerRepo;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
