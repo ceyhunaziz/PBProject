68cc33d83f1f45c5a96a945dd383279b46dd54eb
ceyhunaziz <40896664+ceyhunaziz@users.noreply.github.com>
2019-09-26 10:06:45 +0400

    Testing new Branch

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -9,5 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
+        System.out.println("Hello World!");
     }
 }
